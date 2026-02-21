7620762180916cb5f9795637aced65ebf358c935
Прозоров Владислав <159719052+vladislavprozorov@users.noreply.github.com>
2026-02-21 07:28:40 -0800

    feat/welcome page (#5)
    * feat(welcome): configure routing and lazy loading
    
    * feat(welcome): add \welcome page with navigation to menu
    
    ---------
    
    Co-authored-by: vladislav prozorov <skayterder@vk.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import type { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		pathMatch: "full",
+		redirectTo: "welcome",
+	},
+	{
+		path: "welcome",
+		loadComponent: () =>
+			import("@features/welcome/welcome.component").then(
+				({ Welcome }) => Welcome,
+			),
+	},
+	{
+		path: "menu",
+		loadComponent: () =>
+			import("@features/menu/menu.component").then(({ Menu }) => Menu),
+	},
+];
--- src/app/features/welcome/welcome.component.ts
+++ src/app/features/welcome/welcome.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
 	selector: "app-welcome",
-	imports: [],
+	standalone: true,
+	imports: [RouterLink],
 	templateUrl: "./welcome.html",
 	styleUrl: "./welcome.css",
 })
